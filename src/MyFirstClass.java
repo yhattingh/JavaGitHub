de064e6e182cee6c2ad95aa28061fb85768c478a
yolande <yolande.hattingh@gmail.com>
2022-02-14 21:03:40 +0200

    committing my first class

---
+++ src/MyFirstClass.java
@@ -0,0 +1,11 @@
+
+public class MyFirstClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world - this is Yolande");
+		System.out.println("hello, again world - Yolande checking in...");
+
+	}
+
+}
