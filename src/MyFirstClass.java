361f345794a98e7adcea93518cacc4e04b35c42e
yolande <yolande.hattingh@gmail.com>
2022-02-15 13:55:40 +0200

    committing on 15 FEB

--- src/MyFirstClass.java
+++ src/MyFirstClass.java
@@ -4,7 +4,7 @@ public class MyFirstClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello world - this is Yolande");
-		System.out.println("hello, again world - Yolande checking in...");
+		System.out.println("hello, again world - I am just checking in...");
 
 	}
 
